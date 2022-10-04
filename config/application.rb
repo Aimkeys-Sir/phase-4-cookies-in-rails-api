210042de0908378f64051cf3e35aa7c765796d5b
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-10-04 17:44:41 +0300

    M.A

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,13 @@ module Phase4CookiesInRailsApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.api_only=true
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    config.action_dispatch cookies_same_site_protection= :strict
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -35,13 +42,5 @@ module Phase4CookiesInRailsApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
-
-    # Adding back cookies and session middleware
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
-
-    # Use SameSite=Strict for all cookies to help protect against CSRF
-    config.action_dispatch.cookies_same_site_protection = :strict
   end
 end
